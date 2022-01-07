a6a3938425d066edbd5158a5c615b066b14fd49b
Muhammad Raheel <57173923+mraheel48@users.noreply.github.com>
2022-01-07 11:52:32 +0500

    Initial commit

---
+++ app/src/main/java/com/example/advancesplash12/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.advancesplash12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_AdvanceSplash12)
+        setContentView(R.layout.activity_main)
+    }
+}
